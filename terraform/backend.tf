c4fc7c296f0faf49e75a824efba7f6adb846920c
Srinath Ramamurthy <Srinath.Ramamurthy@optiv.com>
2022-11-29 21:42:15 +0530

    Commiting the changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "nginx-webcontent"
-    key            = "global/s3/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
